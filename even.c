7faf84a72484f5f01881e805f904d242a6f1be4c
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-19 16:22:38 +0530

    even.c
    good

---
+++ even.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int low;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &low, &high);
+
+    printf("Prime numbers between %d and %d are: ", low);
+
+    while (low)
+    {
+        flag = 0;
+
+        for(i = 2; i <= low/2; ++i)
+        {
+            if(low % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        return
+   }
